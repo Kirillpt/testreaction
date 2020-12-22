12b0f5be28646c417d0308f69d79909d517c9c3b
kirillpt <kirp@dismail.de>
2020-12-22 19:55:56 +0300

    add voltage chart

--- PersonData/views.py
+++ PersonData/views.py
@@ -19,13 +19,25 @@ class ChartData(APIView):
     authentication_classes = []
     permission_classes = []
     def get_electro(self):
-        items = [[0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0]]
+        items = [
+                    [0, 0, 0, 0, 0], # all react_time
+                    [0, 0, 0, 0, 0], # female react_time
+                    [0, 0, 0, 0, 0], # male react_time
+                    [0, 0, 0, 0, 0], # all react_time
+                    [0, 0, 0, 0, 0], # female react_time
+                    [0, 0, 0, 0, 0], # male react_time
+            
+                ]
+        cnt_items = [
+                    [0, 0, 0, 0, 0], # all react_time
+                    [0, 0, 0, 0, 0], # female react_time
+                    [0, 0, 0, 0, 0], # male react_time
+                    [0, 0, 0, 0, 0], # all react_time
+                    [0, 0, 0, 0, 0], # female react_time
+                    [0, 0, 0, 0, 0], # male react_time
+            
+                ]
 
-        cnt_items = [[0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0]]
         g = 1 # gender
         for e in Person.objects.filter(test_variant="E"):
             age = e.age
@@ -38,27 +50,52 @@ class ChartData(APIView):
                 cnt_items[g][0] += 1
                 cnt_items[0][0] += 1
                 items[0][0] += e.react_time
+
+                items[g+3][0] += e.e_voltage
+                cnt_items[g+3][0] += 1
+                cnt_items[3][0] += 1
+                items[3][0] += e.e_voltage
             elif age >= 10 and age <= 12:
                 items[g][1] += e.react_time
                 cnt_items[g][1] += 1
                 cnt_items[0][1] += 1
                 items[0][1] += e.react_time
+
+                items[g+3][1] += e.e_voltage
+                cnt_items[g+3][1] += 1
+                cnt_items[3][1] += 1
+                items[3][1] += e.e_voltage
             elif age >= 13 and age <= 14:
                 items[g][2] += e.react_time
                 cnt_items[g][2] += 1
                 cnt_items[0][2] += 1
                 items[0][2] += e.react_time
+
+                items[g+3][2] += e.e_voltage
+                cnt_items[g+3][2] += 1
+                cnt_items[3][2] += 1
+                items[3][2] += e.e_voltage
             elif age >= 15 and age <= 17:
                 items[g][3] += e.react_time
                 cnt_items[g][3] += 1
                 cnt_items[0][3] += 1
                 items[0][3] += e.react_time
+
+                items[g+3][3] += e.e_voltage
+                cnt_items[g+3][3] += 1
+                cnt_items[3][3] += 1
+                items[3][3] += e.e_voltage
             elif age > 17:
                 items[g][4] += e.react_time
                 cnt_items[g][4] += 1
                 cnt_items[0][4] += 1
                 items[0][4] += e.react_time
-        for cg in range(3):
+
+                items[g+3][4] += e.e_voltage
+                cnt_items[g+3][4] += 1
+                cnt_items[3][4] += 1
+                items[3][4] += e.e_voltage
+        for cg in range(6):
             for i in range(5):
                 if cnt_items[cg][i] == 0:
                     continue
@@ -69,9 +106,12 @@ class ChartData(APIView):
         itemsElectro = self.get_electro()
         data = {
                 "labels": labels,
-                "all":  itemsElectro[0],
-                "female": itemsElectro[1],
-                "male": itemsElectro[2],
+                "rall":  itemsElectro[0],
+                "rfemale": itemsElectro[1],
+                "rmale": itemsElectro[2],
+                "vall":  itemsElectro[3],
+                "vfemale": itemsElectro[4],
+                "vmale": itemsElectro[5],
         }
         return Response(data)
 
